845c5e2c30a3f81a3357486f9d1add8e661f2da8
Markus Binsteiner <markus@frkl.io>
2024-04-08 11:12:10 +0200

    test: add test for github download operations

---
+++ tests/job_tests/download_file_bundle_from_github/outputs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from kiara.models.filesystem import KiaraFileBundle
+from kiara.models.values.value import Value
+
+
+def check_downloaded_file(file_bundle: Value):
+
+    assert file_bundle.data_type_name == "file_bundle"
+    assert file_bundle.value_size > 400000
+
+    kiara_file_bundle: KiaraFileBundle = file_bundle.data
+    assert kiara_file_bundle.__class__ == KiaraFileBundle
+
+    assert (
+        "kiara_plugin.core_types-main/.gitignore"
+        in kiara_file_bundle.included_files.keys()
+    )
